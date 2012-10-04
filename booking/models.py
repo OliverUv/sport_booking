7989680eed9cd5e3a6876bc185505e508018a33c
Oliver Uvman <oliver.uvman@gmail.com>
2012-10-04 19:52:27 +0200

    Fix problems in models, add pyc files to ignore.

--- booking/models.py
+++ booking/models.py
@@ -3,12 +3,41 @@ from hvad.models import TranslatableModel, TranslatedFields
 from validators import validate_liuid
 
 
-# Create your models here.
-class User(TranslatableModel):
-    name = models.CharField(max_lenght=255)
+class User(models.Model):
+    name = models.CharField(max_length=255)
     postal_number = models.IntegerField()
     phone_number = models.CharField(max_length=40)
-    liuid = models.CharField(max_lenght=8, validators=[validate_liuid])
+    liuid = models.CharField(max_length=8, validators=[validate_liuid])
+    registration_time = models.DateTimeField(auto_now_add=True)
 
     def __unicode__(self):
         return self.name
+
+
+class Remark(models.Model):
+    time = models.DateField(auto_now_add=True)
+    text = models.TextField()
+    reservation = models.ForeignKey('Reservation', related_name='remarks', blank=True)
+    user = models.ForeignKey(User, related_name='remarks')
+
+
+class ResourceType(TranslatableModel):
+    image = models.ImageField(upload_to='resource_type_images')
+    translations = TranslatedFields(
+            type_name=models.CharField(max_length=255),
+            general_information=models.TextField(blank=True))
+
+
+class Resource(TranslatableModel):
+    longitude = models.DecimalField(max_digits=10, decimal_places=6)
+    latitude = models.DecimalField(max_digits=10, decimal_places=6)
+    translations = TranslatedFields(
+            name=models.CharField(max_length=255),
+            specific_information=models.TextField())
+
+
+class Reservation(models.Model):
+    resource = models.ForeignKey(Resource, related_name='reservations')
+    user = models.ForeignKey(User, related_name='reservations')
+    start = models.DateTimeField()
+    end = models.DateTimeField()
--- sportbooking_frryd/settings.py
+++ sportbooking_frryd/settings.py
@@ -123,7 +123,8 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     'django.contrib.admindocs',
-    'hvad'
+    'hvad',  # Translation of dynamic content
+    'booking'  # Booking system
 )
 
 # A sample logging configuration. The only tangible logging
