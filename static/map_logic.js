a942fc5143019ce885e8d291f75d5cd426db23c1
Oliver Uvman <oliver.uvman@gmail.com>
2013-01-09 03:36:37 +0100

    Halfabstracted maps

---
+++ static/map_logic.js
@@ -0,0 +1,42 @@
+function initializeMap(map_element_id, latitude, longitude) {
+    var mapOptions = {
+	center: new google.maps.LatLng(latitude, longitude),
+	zoom: 16,
+	mapTypeId: google.maps.MapTypeId.SATELLITE,
+	disableDefaultUI: true,
+	panControl: false,
+	zoomControl: false,
+	mapTypeControl: false,
+	scaleControl: false,
+	streetViewControl: false,
+	overviewMapControl: false
+    };
+    var map = new google.maps.Map(document.getElementById(map_element_id), mapOptions);
+    return map;
+}
+
+function addIcon(map, latitude, longitude, image_url, resource_name, resource_url, anchorX, anchorY, zIndex) {
+    var myLatLng = new google.maps.LatLng(latitude, longitude);
+    var image = new google.maps.MarkerImage(image_url,
+	    new google.maps.Size(50, 49),
+	    new google.maps.Point(0,0), // origin, if in sprite with many icons
+	    new google.maps.Point(anchorX, anchorY)); // anchor
+    var marker = new google.maps.Marker({
+          position: myLatLng,
+          map: map,
+          icon: image,
+	  title: resource_name,
+	  zIndex: zIndex
+      });
+    google.maps.event.addListener(marker, 'click', function() {
+	window.location = resource_url;
+    });
+}
+
+function addArrowIcon(map, latitude, longitude, image_url, resource_name, resource_url) {
+    addIcon(map, latitude, longitude, image_url, resource_name, resource_url, 25, 49, 2);
+}
+
+function addResourceIcon(map, latitude, longitude, image_url, resource_name, resource_url) {
+    addIcon(map, latitude, longitude, image_url, resource_name, resource_url, 25, 25, 1);
+}
