a6e0bdae36acba89c4b45adb1e0f5cf50597a56e
Oliver Uvman <oliver.uvman@gmail.com>
2013-01-09 02:37:15 +0100

    Cleanup

--- static/common.js
+++ static/common.js
@@ -59,14 +59,14 @@ function showAjaxFailure(data) {
     });
 }
 
-function resize_map(map_element_id, related_size_element) {
+function fill_element(resize_element_id, related_size_element) {
     var win_width = $(window).width();
     if (win_width < 767) {
 	var win_height = $(window).height();
 	var other_height = $("#" + related_size_element).height();
-	$("#" + map_element_id).height(win_height - other_height);
+	$("#" + resize_element_id).height(win_height - other_height);
     } else {
-	$("#" + map_element_id).height(500);
+	$("#" + resize_element_id).height(500);
     }
 };
 
